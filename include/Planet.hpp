0d2167d7988c5454b2f61719dca0fc12ef29debd
chrisPyr <smile51293@gmail.com>
2021-11-01 10:02:31 +0800

    [INCLUDE] update Planet.hpp

--- include/Planet.hpp
+++ include/Planet.hpp
@@ -5,10 +5,14 @@
 #include <vector>
 
 class Planet {
-  using std::vector<double> vec_dou private : double m_mass;
-  std::vector<double> m_pos;
-  std::vector<double> m_veloc; // velocity along x, y axis
-  std::vector<double> m_acc;   // accelerate along x, y axis
+  using std::vector<double> vec_dou;
+
+private:
+  double m_mass;
+
+  vec_dou m_pos;
+  vec_dou m_veloc; // velocity along x, y axis
+  vec_dou m_acc;   // accelerate along x, y axis
 public:
   Planet(double mass, vec_dou pos, vec_dou veloc, vec_dou acc)
       : m_mass{mass}, m_pos{pos}, m_veloc{veloc}, m_acc{acc} {}
