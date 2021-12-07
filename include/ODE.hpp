e58bf3edb9c8e91ca8edb16a74ea4b5748e2eb17
chrisPyr <smile51293@gmail.com>
2021-12-07 20:54:49 +0800

    [Include] rename some function and add constructor

--- include/ODE.hpp
+++ include/ODE.hpp
@@ -3,8 +3,11 @@
 
 #include "SolarSys.hpp"
 class ODE {
+private:
+  void Euler(SolarSys &sys);
+  void RungeKutta(SolarSys &sys);
+
 public:
-  void naiveUpdate(SolarSys &sys);
-  void update(SolarSys &sys, size_t itr);
+  void update(SolarSys &sys, size_t itr, size_t method);
 };
 #endif
--- include/Planet.hpp
+++ include/Planet.hpp
@@ -18,6 +18,19 @@ private:
 public:
   Planet(std::string name, double mass, vec_dou pos, vec_dou veloc, vec_dou acc)
       : m_name{name}, m_mass{mass}, m_pos{pos}, m_veloc{veloc}, m_acc{acc} {}
+  Planet(Planet const &other)
+      : m_name{other.m_name}, m_mass{other.m_mass}, m_pos{other.m_pos}, m_veloc{other.m_veloc}, m_acc{other.m_acc}{
+  }
+  Planet &operator=(Planet const &other){
+    if(&other != this){
+      m_name = other.m_name;
+      m_mass = other.m_mass;
+      m_pos = other.m_pos;
+      m_veloc = other.m_veloc;
+      m_acc = other.m_acc;
+    }
+    return *this;
+  }
   ~Planet() = default;
   std::string name() { return m_name; };
   double mass() { return m_mass; };
