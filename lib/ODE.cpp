4f8085383010dc8e70d431aa82c6d821f85710a3
chrisPyr <smile51293@gmail.com>
2021-12-21 19:09:02 +0800

    [lib] Runge Kutta OK

--- lib/ODE.cpp
+++ lib/ODE.cpp
@@ -7,18 +7,36 @@
 #include <utility>
 
 const long double G = 6.67408e-20L;
-const long double time_interval = 100;
+const long double time_interval = 60;
 
-void ODE::update(SolarSys &sys, long long itr, size_t method) {
+std::vector<std::vector<long double> > ODE::update(SolarSys &sys, long long itr, size_t method) {
   const long double itr_step = 1 / time_interval;
+  std::vector<std::vector<long double> >  pos_table(sys.size() * 366);
+  const size_t system_size = sys.size();
   if (1 == method) {
     for (long long i = 0; i < itr * itr_step; ++i) {
       Euler(sys);
+      if(i == 0 || (i+1)%(24*60) == 0){
+      for (size_t k = 0; k < sys.size(); ++k){
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[0]);
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[1]);
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[2]);
+      }
+      }
     }
+    return pos_table;
   } else if (2 == method) {
     for (long long i = 0; i < itr * itr_step; ++i) {
       RungeKutta(sys);
+      if(i == 0 || (i+1)%(24*60) == 0){
+      for (size_t k = 0; k < sys.size(); ++k){
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[0]);
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[1]);
+        pos_table[system_size*(i+1)/(24*60) + k].push_back(sys(k).pos()[2]);
+      }
+      }
     }
+    return pos_table;
   } else {
     throw "invalid method choice!!";
   }
@@ -28,46 +46,58 @@ void ODE::Euler(SolarSys &sys) {
 
   long double delta_x = 0.0;
   long double delta_y = 0.0;
-  auto vec_table = std::make_unique<std::pair<long double, long double>[]>(
+  long double delta_z = 0.0;
+  //auto vec_table = std::make_unique<std::pair<long double, long double>[]>(
+  //    sys.size() * sys.size());
+  auto vec_table = std::make_unique<std::vector<long double>[]>(
       sys.size() * sys.size());
 
 //#pragma omp parallel for private(delta_x, delta_y)
   for (size_t x = 0; x < sys.size(); ++x) {
     for (size_t y = 0; y < sys.size(); ++y) {
       if (x == y) {
-        (vec_table[x * sys.size() + y]).first = (sys(x).veloc())[0];
-        (vec_table[x * sys.size() + y]).second = (sys(x).veloc())[1];
+        (vec_table[x * sys.size() + y]).push_back( (sys(x).veloc())[0]);
+        (vec_table[x * sys.size() + y]).push_back((sys(x).veloc())[1]);
+        (vec_table[x * sys.size() + y]).push_back((sys(x).veloc())[2]);
         continue;
       }
 
       delta_x = (sys(y).pos())[0] - (sys(x).pos())[0];
       delta_y = (sys(y).pos())[1] - (sys(x).pos())[1];
-      long double r = delta_x * delta_x + delta_y * delta_y;
-      (vec_table[x * sys.size() + y]).first =
-          (G * (sys(y).mass()) / r * (delta_x / std::sqrt(r))) * time_interval;
-      (vec_table[x * sys.size() + y]).second =
-          (G * (sys(y).mass()) / r * (delta_y / std::sqrt(r))) * time_interval;
+      delta_z = (sys(y).pos())[2] - (sys(x).pos())[2];
+      long double r = delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
+      (vec_table[x * sys.size() + y]).push_back(
+          (G * (sys(y).mass()) / r * (delta_x / std::sqrt(r))) * time_interval);
+      (vec_table[x * sys.size() + y]).push_back(
+          (G * (sys(y).mass()) / r * (delta_y / std::sqrt(r))) * time_interval);
+      (vec_table[x * sys.size() + y]).push_back(
+          (G * (sys(y).mass()) / r * (delta_z / std::sqrt(r))) * time_interval);
     }
   }
 
 
   long double x_vec_sum = 0.0;
   long double y_vec_sum = 0.0;
+  long double z_vec_sum = 0.0;
 
 //#pragma omp parallel for private(x_vec_sum, y_vec_sum)
   for (size_t idx = 0; idx < sys.size(); ++idx) {
     x_vec_sum = 0.0;
     y_vec_sum = 0.0;
+    z_vec_sum = 0.0;
     (sys(idx).pos())[0] += ((sys(idx).veloc())[0] * time_interval);
     (sys(idx).pos())[1] += ((sys(idx).veloc())[1] * time_interval);
+    (sys(idx).pos())[2] += ((sys(idx).veloc())[2] * time_interval);
     for (size_t vec = 0; vec < sys.size(); ++vec) {
-      x_vec_sum += (vec_table[idx * sys.size() + vec]).first;
-      y_vec_sum += (vec_table[idx * sys.size() + vec]).second;
+      x_vec_sum += (vec_table[idx * sys.size() + vec])[0];
+      y_vec_sum += (vec_table[idx * sys.size() + vec])[1];
+      z_vec_sum += (vec_table[idx * sys.size() + vec])[2];
     }
     //(sys(idx).pos())[0] += (x_vec_sum * time_interval);
     //(sys(idx).pos())[1] += (y_vec_sum * time_interval);
     (sys(idx).veloc())[0] = x_vec_sum;
     (sys(idx).veloc())[1] = y_vec_sum;
+    (sys(idx).veloc())[2] = z_vec_sum;
   }
 
   // delete[] vec_table;
@@ -154,11 +184,13 @@ SolarSys RK4Step_sub(SolarSys sys_rep, SolarSys base, size_t type){
   for(size_t x =0 ;x<system_size;++x){
     sys_rep(x).pos()[0] = base(x).pos()[0] + sys_rep(x).veloc()[0] * time_in;
     sys_rep(x).pos()[1] = base(x).pos()[1] + sys_rep(x).veloc()[1] * time_in;
+    sys_rep(x).pos()[2] = base(x).pos()[2] + sys_rep(x).veloc()[2] * time_in;
   }
   long double delta_x = 0.0;
   long double delta_y = 0.0;
+  long double delta_z = 0.0;
 
-  auto acc_table = std::make_unique<std::pair<long double, long double>[]>(
+  auto acc_table = std::make_unique<std::vector<long double>[]>(
       system_size * system_size);
 
 //#pragma omp parallel
@@ -168,18 +200,22 @@ SolarSys RK4Step_sub(SolarSys sys_rep, SolarSys base, size_t type){
     for (size_t y = 0; y < system_size; ++y) {
       const size_t index = x * system_size + y;
       if (x == y) {
-        (acc_table[index]).first = 0.0;
-        (acc_table[index]).second = 0.0;
+        (acc_table[index]).push_back(0.0);
+        (acc_table[index]).push_back(0.0);
+        (acc_table[index]).push_back(0.0);
         continue;
       }
 
       delta_x = (sys_rep(y).pos())[0] - (sys_rep(x).pos())[0];
       delta_y = (sys_rep(y).pos())[1] - (sys_rep(x).pos())[1];
-      long double r = delta_x * delta_x + delta_y * delta_y;
-      (acc_table[index]).first =
-          (G * (sys_rep(y).mass()) / r * (delta_x / std::sqrt(r)));
-      (acc_table[index]).second =
-          (G * (sys_rep(y).mass()) / r * (delta_y / std::sqrt(r)));
+      delta_z = (sys_rep(y).pos())[2] - (sys_rep(x).pos())[2];
+      long double r = delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
+      (acc_table[index]).push_back(
+          (G * (sys_rep(y).mass()) / r * (delta_x / std::sqrt(r))));
+      (acc_table[index]).push_back(
+          (G * (sys_rep(y).mass()) / r * (delta_y / std::sqrt(r))));
+      (acc_table[index]).push_back(
+          (G * (sys_rep(y).mass()) / r * (delta_z / std::sqrt(r))));
     }
   }
 //#pragma omp for
@@ -187,28 +223,31 @@ SolarSys RK4Step_sub(SolarSys sys_rep, SolarSys base, size_t type){
   for(size_t x = 0; x < system_size; ++x){
     sys_rep(x).veloc()[0] = base(x).veloc()[0] + sys_rep(x).acc()[0] * time_in;
     sys_rep(x).veloc()[1] = base(x).veloc()[1] + sys_rep(x).acc()[1] * time_in;
+    sys_rep(x).veloc()[2] = base(x).veloc()[2] + sys_rep(x).acc()[2] * time_in;
   }
 
-  long double x_acc_sum, y_acc_sum;
+  long double x_acc_sum, y_acc_sum, z_acc_sum;
 //#pragma omp for private(x_acc_sum, y_acc_sum)
   for (size_t idx = 0; idx < system_size; ++idx) {
     x_acc_sum = 0.0;
     y_acc_sum = 0.0;
+    z_acc_sum = 0.0;
     for (size_t vec = 0; vec < system_size; ++vec) {
       const size_t index = idx * system_size + vec;
-      x_acc_sum += (acc_table[index]).first;
-      y_acc_sum += (acc_table[index]).second;
+      x_acc_sum += (acc_table[index])[0];
+      y_acc_sum += (acc_table[index])[1];
+      z_acc_sum += (acc_table[index])[2];
     }
     (sys_rep(idx).acc())[0] = x_acc_sum;
     (sys_rep(idx).acc())[1] = y_acc_sum;
+    (sys_rep(idx).acc())[2] = z_acc_sum;
   }
 //}
 
   return sys_rep;
 }
 
-void ODE::RungeKutta(SolarSys &sys) {
-
+void ODE::RungeKutta(SolarSys &sys) { 
   // RK4 parameters
   //std::cout.precision(17);
   //SolarSys k0 = sys;                // veloc k1
@@ -247,39 +286,47 @@ void ODE::RungeKutta(SolarSys &sys) {
   const size_t system_size = sys.size();
   long double delta_x = 0.0;
   long double delta_y = 0.0;
+  long double delta_z = 0.0;
 
-  auto acc_table = std::make_unique<std::pair<long double, long double>[]>(
+  auto acc_table = std::make_unique<std::vector<long double>[]>(
       system_size * system_size);
 
   for (size_t x = 0; x < system_size; ++x) {
     for (size_t y = 0; y < system_size; ++y) {
       const size_t index = x * system_size + y;
       if (x == y) {
-        (acc_table[index]).first = 0.0;
-        (acc_table[index]).second = 0.0;
+        (acc_table[index]).push_back(0.0);
+        (acc_table[index]).push_back(0.0);
+        (acc_table[index]).push_back(0.0);
         continue;
       }
 
       delta_x = (sys(y).pos())[0] - (sys(x).pos())[0];
       delta_y = (sys(y).pos())[1] - (sys(x).pos())[1];
-      long double r = delta_x * delta_x + delta_y * delta_y;
-      (acc_table[index]).first =
-          (G * (sys(y).mass()) / r * (delta_x / std::sqrt(r)));
-      (acc_table[index]).second =
-          (G * (sys(y).mass()) / r * (delta_y / std::sqrt(r)));
+      delta_z = (sys(y).pos())[2] - (sys(x).pos())[2];
+      long double r = delta_x * delta_x + delta_y * delta_y + delta_z * delta_z;
+      (acc_table[index]).push_back(
+          (G * (sys(y).mass()) / r * (delta_x / std::sqrt(r))));
+      (acc_table[index]).push_back(
+          (G * (sys(y).mass()) / r * (delta_y / std::sqrt(r))));
+      (acc_table[index]).push_back(
+          (G * (sys(y).mass()) / r * (delta_z / std::sqrt(r))));
     }
   }
-  long double x_acc_sum, y_acc_sum;
+  long double x_acc_sum, y_acc_sum, z_acc_sum;
   for (size_t idx = 0; idx < system_size; ++idx) {
     x_acc_sum = 0.0;
     y_acc_sum = 0.0;
+    z_acc_sum = 0.0;
     for (size_t vec = 0; vec < system_size; ++vec) {
       const size_t index = idx * system_size + vec;
-      x_acc_sum += (acc_table[index]).first;
-      y_acc_sum += (acc_table[index]).second;
+      x_acc_sum += (acc_table[index])[0];
+      y_acc_sum += (acc_table[index])[1];
+      z_acc_sum += (acc_table[index])[2];
     }
     (sys(idx).acc())[0] = x_acc_sum;
     (sys(idx).acc())[1] = y_acc_sum;
+    (sys(idx).acc())[2] = z_acc_sum;
   }
   std::cout.precision(17);
 
@@ -296,11 +343,17 @@ void ODE::RungeKutta(SolarSys &sys) {
     (sys(i).pos())[1] += ((k1(i).veloc())[1] + 2 * (k2(i).veloc())[1] +
                           2 * (k3(i).veloc())[1] + (k4(i).veloc())[1]) *
                          time_interval / 6.0;
+    (sys(i).pos())[2] += ((k1(i).veloc())[2] + 2 * (k2(i).veloc())[2] +
+                          2 * (k3(i).veloc())[2] + (k4(i).veloc())[2]) *
+                         time_interval / 6.0;
     (sys(i).veloc())[0] += ((k1(i).acc())[0] + 2 * (k2(i).acc())[0] +
                             2 * (k3(i).acc())[0] + (k4(i).acc())[0]) *
                            time_interval / 6.0;
     (sys(i).veloc())[1] += ((k1(i).acc())[1] + 2 * (k2(i).acc())[1] +
                             2 * (k3(i).acc())[1] + (k4(i).acc())[1]) *
+                           time_interval / 6.0;
+    (sys(i).veloc())[2] += ((k1(i).acc())[2] + 2 * (k2(i).acc())[2] +
+                            2 * (k3(i).acc())[2] + (k4(i).acc())[2]) *
                            time_interval / 6.0;
   }
 }
