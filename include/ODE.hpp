8314bd8fab66b20afbe762f236f4a8a95ef9d354
chrisPyr <smile51293@gmail.com>
2021-12-21 19:08:34 +0800

    [include] Runge Kutta OK

--- include/ODE.hpp
+++ include/ODE.hpp
@@ -2,12 +2,13 @@
 #define ODE_HPP
 
 #include "SolarSys.hpp"
+#include <vector>
 class ODE {
 private:
   void Euler(SolarSys &sys);
   void RungeKutta(SolarSys &sys);
 
 public:
-  void update(SolarSys &sys, long long itr, size_t method);
+  std::vector<std::vector<long double> > update(SolarSys &sys, long long itr, size_t method);
 };
 #endif
